88e4aadd07c6e3933f9eca381bab074fe08ed95e
Nahom Teshome <160412453+Nahom-Teshome@users.noreply.github.com>
2025-01-29 16:17:23 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-// https://vite.dev/config/
-dotenv.config()
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    host:'0.0.0.0',
-    proxy: {
-        '/api': {
-            target:process.env.VITE_API_URL,
-            // target:'http://localhost:3000',
-            changeOrigin: true,
-          },
-      },
-      '/socket.io': { // Proxy requests to /socket.io
-        // target: 'http://localhost:3000', // Your backend server address
-        target:process.env.VITE_API_URL, // Your backend server address
-        ws: true, // Enable WebSocket proxying
-        changeOrigin: true // Important for CORS
-      },
-  }
-})
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import dotenv from 'dotenv'
+// // https://vite.dev/config/
+// dotenv.config()
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     host:'0.0.0.0',
+//     proxy: {
+//         '/api': {
+//             target:process.env.VITE_API_URL,
+//             // target:'http://localhost:3000',
+//             changeOrigin: true,
+//           },
+//       },
+//       '/socket.io': { // Proxy requests to /socket.io
+//         // target: 'http://localhost:3000', // Your backend server address
+//         target:process.env.VITE_API_URL, // Your backend server address
+//         ws: true, // Enable WebSocket proxying
+//         changeOrigin: true // Important for CORS
+//       },
+//   }
+// })
