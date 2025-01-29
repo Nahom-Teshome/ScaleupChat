6ffb7f257ef985be57beb042a3de91e4fa1729e9
Nahom Teshome <160412453+Nahom-Teshome@users.noreply.github.com>
2025-01-29 13:17:58 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,14 +8,14 @@ export default defineConfig({
     host:'0.0.0.0',
     proxy: {
         '/api': {
-            target:'http://192.168.39.47:3000',
+            target:'VITE_API_KEY',
             // target:'http://localhost:3000',
             changeOrigin: true,
           },
       },
       '/socket.io': { // Proxy requests to /socket.io
         // target: 'http://localhost:3000', // Your backend server address
-        target: 'http://192.168.39.47:3000', // Your backend server address
+        target: 'VITE_API_KEY', // Your backend server address
         ws: true, // Enable WebSocket proxying
         changeOrigin: true // Important for CORS
       },
