e3ab423ba0bdd5158bef0c23ff55fe0cf24c1438
Nahom Teshome <160412453+Nahom-Teshome@users.noreply.github.com>
2025-01-29 13:39:33 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from 'dotenv'
 // https://vite.dev/config/
+dotenv.config()
 export default defineConfig({
   plugins: [react()],
   server:{
     host:'0.0.0.0',
     proxy: {
         '/api': {
-            target:'VITE_API_KEY',
+            target:process.env.VITE_API_URL,
             // target:'http://localhost:3000',
             changeOrigin: true,
           },
       },
       '/socket.io': { // Proxy requests to /socket.io
         // target: 'http://localhost:3000', // Your backend server address
-        target: 'VITE_API_KEY', // Your backend server address
+        target:process.env.VITE_API_URL, // Your backend server address
         ws: true, // Enable WebSocket proxying
         changeOrigin: true // Important for CORS
       },
