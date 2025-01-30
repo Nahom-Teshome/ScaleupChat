0e09259cc0d53f6da3236a960a85855c8b67d03c
Nahom-Teshome <160412453+Nahom-Teshome@users.noreply.github.com>
2025-01-30 11:45:02 +0300

    Update useLogin.js
    credentials set to include in login

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -14,6 +14,7 @@ export function useLogin(){
                     const res = await fetch(`${import.meta.env.VITE_API_URL}/api/user/login`,{
                         method:'POST',
                         headers:{'Content-Type':'application/json'},
+                        credentials:"include",
                         body:JSON.stringify({email,password})
                     })
                     
