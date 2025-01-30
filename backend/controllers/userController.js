175dafbf4a7ed30412438047a28d55b241eb785e
Nahom-Teshome <160412453+Nahom-Teshome@users.noreply.github.com>
2025-01-30 10:29:20 +0300

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,7 @@ async function userSignup(req,res){
                  httpOnly:true,//no javascript manipulationddd
                  secure: true, //for production 
                  sameSite:"none", // must be accessed only when request comes from the same path
-                //  domain:".onrender.com",
+                 domain:".onrender.com",
                  maxAge:(60000 * 60 *24)// expiry date for 1 day 60000seconds * 60 * 24
              })
              // below we send back the user object with necessary data
@@ -54,7 +54,7 @@ async function userLogin(req,res){
                 httpOnly:true,
                  secure:true, 
                  sameSite:"none",
-                //  domain:".onrender.com",
+                 domain:".onrender.com",
                maxAge:(60000 * 60 *24)
              })
         console.log("cookies sent : ", cook)
